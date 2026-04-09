3773b9dec57fb61e8c88ca26716c3bfb9202dd40
RaghaNandini <bainaraghanandini@gmail.com>
2026-04-09 13:27:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import hashlib
 
 app = FastAPI()
 
+
+# Function to generate SHA256 token
 def generate(text):
     return hashlib.sha256(text.encode()).hexdigest()
 
+
+# Pydantic model
 class TextInput(BaseModel):
     text: str
 
 
+# Webpage endpoint
 @app.get("/", response_class=HTMLResponse)
 def welcome():
     return """
@@ -19,28 +24,39 @@ def welcome():
     <head>
         <title>Welcome</title>
         <style>
-            body {
-                font-family: Arial;
-                background-color: #111;
-                color: white;
-                text-align: center;
-                padding-top: 100px;
+            body{
+                background-color:#111;
+                color:white;
+                font-family:Arial;
+                text-align:center;
+                padding-top:120px;
             }
-            h1 { color: #00ffcc; }
-            p { font-size:18px; }
+            h1{
+                color:#00ffcc;
+                font-size:40px;
+            }
+            p{
+                font-size:18px;
+            }
         </style>
     </head>
 
     <body>
+
         <h1>Welcome to the webpage, Baina Ragha Nandini!</h1>
+
         <p>"The only way to do great work is to love what you do." – Steve Jobs</p>
+
         <h2>Containerized Python API</h2>
-        <p>This API generates SHA256 tokens using FastAPI</p>
+
+        <p>This application exposes a JSON API using FastAPI.</p>
+
     </body>
     </html>
     """
 
 
+# API endpoint
 @app.post("/generate")
 def generate_token(data: TextInput):
     text = data.text
